eb719022f3db8d374ebac1f1a29fe0a6e03f503b
lucasgomesdacruz <lucaass1997rp@gmail.com>
2025-05-08 21:30:04 -0300

    fix: arquivo env ruim

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 from os import environ # Traz para o arquivo o acesso as variáveis de ambiente
-from dotenv import load_dotenv # Traz a funçõa para carregar as variaveis de ambiente nesse arquivo 
+from dotenv import load_dotenv # Traz a funçõa para carregar as variaveis de ambiente nesse arquivo
+import os
 
 
 load_dotenv() # Carrega as variaveis de ambiente para este arquivo
 
 class Config():    
     # SQLALCHEMY_DATABASE_URI = environ.get('URL_DATABASE_PROD')
-    SQLALCHEMY_DATABASE_URI = environ.get('URL_DATABASE_DEV')
+    SQLALCHEMY_DATABASE_URI = environ.get('URL_DATABASE_DEV1')
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
+    print("DATABASE URI:", environ.get("URL_DATABASE_DEV1"))
+    
 
