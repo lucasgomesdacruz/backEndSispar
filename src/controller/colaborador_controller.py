92193884dab3b67d20e7ef829ddb2fbd259dfdb2
lucasgomesdacruz <lucaass1997rp@gmail.com>
2025-05-09 10:35:39 -0300

    add: retira

--- src/controller/colaborador_controller.py
+++ src/controller/colaborador_controller.py
@@ -167,8 +167,8 @@ def pegar_perfil_colaborador():
         return jsonify({'mensagem': 'Colaborador não logado'}), 401
 
     # Busca o colaborador no banco de dados usando o ID da sessão
-    colaborador = db.session.get(Colaborador, colaborador_id)
-    # colaborador = db.session.query(Colaborador).filter_by(id=colaborador_id).first()
+    # colaborador = db.session.get(Colaborador, colaborador_id)
+    colaborador = db.session.query(Colaborador).filter_by(id=colaborador_id).first()
 
     # Se o colaborador não for encontrado, retorna 404 (não encontrado)
     if not colaborador:
