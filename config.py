fdf237b89ac3fe59bf8066844fd94bc8cc99b021
lucasgomesdacruz <lucaass1997rp@gmail.com>
2025-05-12 23:17:54 -0300

    feat: adiciona rota para pegar reembolso de user

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ from dotenv import load_dotenv # Traz a funçõa para carregar as variaveis de a
 load_dotenv() # Carrega as variaveis de ambiente para este arquivo
 
 class Config():    
-    SQLALCHEMY_DATABASE_URI = environ.get('URL_DATABASE_PROD1')
+    SQLALCHEMY_DATABASE_URI = environ.get('URL_DATABASE_PROD')
     # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') 
     print("DATABASE URI:", SQLALCHEMY_DATABASE_URI)  
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
-    print("DATABASE URI:", environ.get("URL_DATABASE_PROD1"))
+    print("DATABASE URI:", environ.get("URL_DATABASE_PROD"))
     
 
